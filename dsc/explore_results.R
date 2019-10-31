a137f44535cb1e933b1300a377f2c4dc1af70b16
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-10-31 16:24:07 -0500

    Added lasso fit module with lambda_est_method module parameter.

--- dsc/explore_results.R
+++ dsc/explore_results.R
@@ -1,3 +1,9 @@
 library(dscrutils)
 dsc <- dscquery("mr_ash",
-                targets = c("simulate","simulate.s","simulate.sigma","fit"))
+                targets = c("simulate","simulate.s","simulate.sigma",
+                            "fit","fit.lambda_est_method"))
+dsc <- transform(dsc,
+                 simulate = factor(simulate),
+                 fit      = factor(fit),
+                 fit.lambda_est_method = factor(fit.lambda_est_method))
+summary(dsc[c("simulate","fit","fit.lambda_est_method")])
