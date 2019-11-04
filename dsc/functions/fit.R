4fea39f0c196a67aa7ae39f6459828d601a99591
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-11-04 10:25:57 -0600

    Made some minor adjustments to the DSC throughout.

--- dsc/functions/fit.R
+++ dsc/functions/fit.R
@@ -2,31 +2,25 @@
 # strength (i.e., the normal prior on the regression coefficients)
 # using cross-validation.
 fit_ridge = function(X, y, lambda = c("lambda.min", "lambda.1se"),
-                     standardize = FALSE) {
-  lambda = match.arg(lambda)
-  fit_lasso(X, y, lambda, standardize, alpha = 0)
-}
+                     standardize = FALSE)
+  fit_glmnet(X, y, match.arg(lambda), standardize, alpha = 0)
 
 # Fit a Lasso model to the data (or, more generally, an Elastic Net
 # model with a fixed setting of alpha), and estimate the penalty
 # strength (lambda) using cross-validation.
 fit_lasso = function(X, y, lambda = c("lambda.min", "lambda.1se"),
-                     standardize = FALSE, alpha = 1) {
-  lambda = match.arg(lambda)
-  timing = system.time(
-    fit <- glmnet::cv.glmnet(x = X, y = y, standardize = standardize,
-                             alpha = alpha))
-  b = drop(coef(fit, s = fit[[lambda]]))
-  return(list(fit = fit, mu = b[1], beta = b[-1], timing = timing["elapsed"]))
-}
+                     standardize = FALSE)
+  fit_glmnet(X, y, match.arg(lambda), standardize, alpha = 1)
 
 # Fit an Elastic Net model to the data, and estimate the Elastic Net
 # parameters (penalty strength, "lambda", and mixing parameter,
 # "alpha") using cross-validation.
 fit_elastic_net = function(X, y, lambda = c("lambda.min", "lambda.1se"),
-                           standardize = FALSE) {
-  alpha  = seq(0,1,0.1)
+                           standardize = FALSE, alpha = seq(0,1,0.1)) {
   lambda = match.arg(lambda)
+
+  # Fit an Elastic Net model for each candidate setting of the mixing
+  # parameter, alpha.
   n      = length(alpha)
   out    = vector("list",n)
   cv.err = double(n)
@@ -40,15 +34,24 @@ fit_elastic_net = function(X, y, lambda = c("lambda.min", "lambda.1se"),
 
   # Return the results for the elastic net model producing the lowest 
   i = which.min(cv.err)
-    
   return(c(out[[i]][c("fit","mu","beta")],
-           list(timing = timing["elapsed"], alpha = alpha[i])))
+           list(timing = timing["elapsed"],
+                alpha  = alpha[i])))
+}
+
+# Fit an Elastic Net model, in which the penalty strength (lambda) is
+# estimated using cross-validation.
+fit_glmnet = function(X, y, lambda, standardize, alpha) {
+  timing = system.time(
+    fit <- glmnet::cv.glmnet(x = X, y = y, standardize = standardize,
+                             alpha = alpha))
+  b = drop(coef(fit, s = fit[[lambda]]))
+  return(list(fit = fit, mu = b[1], beta = b[-1], timing = timing["elapsed"]))
 }
 
 # Compute a fully-factorized variational approximation for Bayesian
 # variable selection in linear regression.
-fit_varbvs = function(X, y) {
-  logodds = seq(-log10(ncol(X)), 1, length.out = 40)
+fit_varbvs = function(X, y, logodds = seq(-log10(ncol(X)),1,length.out = 40)) {
   timing = system.time(
     fit <- varbvs::varbvs(X, Z = NULL, y, logodds = logodds, verbose = FALSE))
   b = drop(coef(fit)[,"averaged"])
