fca09b0f294401ffff609135ab745efe821f8b71
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-11-04 14:39:45 -0600

    Minor edit to explore_dsc_results analysis.
    Former-commit-id: 6ba51d4774aee5a554a674ba4a1233d992424ed0

--- dsc/explore_results.R
+++ dsc/explore_results.R
@@ -1,22 +1,3 @@
-library(dscrutils)
-library(ggplot2)
-library(cowplot)
-
-# Load the DSC results.
-dsc <- dscquery("mr_ash",
-                targets = c("simulate","simulate.s","simulate.sigma",
-                            "fit","fit.lambda_est_method","score.err"))
-
-# Prepare the data for plotting.
-rows <- which(is.na(dsc$fit.lambda_est_method))
-dsc$fit.lambda_est_method[rows] <- ""
-dsc <- transform(dsc,fit = paste(fit,fit.lambda_est_method))
-dsc <- transform(dsc,
-                 simulate              = factor(simulate),
-                 simualte.s            = factor(simulate.s),
-                 fit                   = factor(fit),
-                 fit.lambda_est_method = factor(fit.lambda_est_method))
-
 # Summarize the prediction error separately for each setting of "s", the
 # number of simulated effects.
 p <- ggplot(dsc,aes(x = fit,y = score.err,fill = fit)) +
