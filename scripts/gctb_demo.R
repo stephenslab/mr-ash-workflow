25e7c2ab579244543d4a5a412668bfdd5ea3e585
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-04-23 13:16:31 -0500

    Working on steps to run BayesR method in gctb_demo.R.

--- scripts/gctb_demo.R
+++ scripts/gctb_demo.R
@@ -1,5 +1,6 @@
 # TO DO: Explain here what this script does, and how to use it.
 library(glmnet)
+source("../code/gctb.R")
 
 # SIMULATE DATA
 # -------------
@@ -27,6 +28,34 @@ yest <- drop(predict(fit.glmnet,X,s = fit.glmnet$lambda.min))
 plot(y,yest,pch = 20,col = "darkblue")
 abline(a = 0,b = 1,lty = "dotted",col = "magenta")
 
-# FIT BAYESR MODEL
+# FIT BayesR MODEL
 # ----------------
-# TO DO.
+# Create the "fam" file.
+dat <- data.frame(fid   = paste0("s",1:n),
+                  iid   = paste0("s",1:n),
+                  pid   = 0,
+                  mid   = 0,
+                  sex   = 0,
+                  pheno = -9)
+write.table(dat,"sim.fam",sep = " ",row.names = FALSE,col.names = FALSE,
+            quote = FALSE)
+
+# Create the "map" file.
+dat <- data.frame(chr = 1,
+                  id  = paste0("rs",1:p),
+                  cM  = 0,
+                  bp  = 1000*(1:p))
+write.table(dat,"sim.bim",sep = " ",row.names = FALSE,col.names = FALSE,
+            quote = FALSE)
+
+# Create the "ped" file.
+
+
+# Create the PLINK bed/bim/fam fileset.
+
+
+#
+#   gctb --bfile 1000G_eur_chr22 --pheno test.phen \
+#     --bayes R --seed 1 --gamma 0,0.01,0.1,1 --pi 0.25,0.25,0.25,0.25
+#     --chain-length 1500 --burn-in 500 --out test 
+#
