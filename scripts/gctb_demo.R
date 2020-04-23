7f34e1812beef73d6a4c5543347a857f2063dcc5
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-04-23 12:06:13 -0500

    Added step to gctb_demo.R to fit glmnet model.

--- scripts/gctb_demo.R
+++ scripts/gctb_demo.R
@@ -7,19 +7,25 @@ set.seed(1)
 n <- 500
 p <- 2000
 X <- matrix(rnorm(n*p),n,p)
+X <- scale(X,center = TRUE,scale = FALSE)
 
-# Generate regression coefficients so that only 20 of the predictors
+# Generate regression coefficients so that only 5 of the predictors
 # affect the response.
-i    <- sample(p,20)
+i    <- sample(p,5)
 b    <- rep(0,p)
-b[i] <- rnorm(na)
+b[i] <- rnorm(5)
 
 # Generate the responses.
-y <- drop(X %*% b + rnorm(n))
+y <- drop(X %*% b + 2*rnorm(n))
 
 # FIT ELASTIC NET MODEL
 # ---------------------
-# TO DO.
+# Fit the elastic net model, and use it to estimate the responses in
+# the training data.
+fit.glmnet <- cv.glmnet(X,y,alpha = 0.95)
+yest <- drop(predict(fit.glmnet,X,s = fit.glmnet$lambda.min))
+plot(y,yest,pch = 20,col = "darkblue")
+abline(a = 0,b = 1,lty = "dotted",col = "magenta")
 
 # FIT BAYESR MODEL
 # ----------------
